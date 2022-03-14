51732c82073ffdc898812c7e1627f7ef3725e73a
ankitaraghuwanshi <bittanraghu@gmail.com>
2022-03-14 22:35:02 +0530

    UC1-FlipCoin

---
+++ BasicCorePrograme/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+using System;
+
+namespace BasicCorePrograme
+{
+
+    class CoinFlip
+    {
+        public static void Main(string[] args)
+        {
+            int coinHead = 1; // constant
+            Random random = new Random();
+            int coincheck = random.Next(0, 2);
+            if (coincheck == coinHead)
+            {
+                Console.WriteLine("coin is Head");
+
+            }
+            else
+            {
+                Console.WriteLine("coin is tails");
+
+            }
+        }
+
+    }
+}
