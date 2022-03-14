8317e69ec14f5b85ef7a484be302b5374f6601ec
ankitaraghuwanshi <bittanraghu@gmail.com>
2022-03-15 00:47:11 +0530

    UC8-EvenOdd

--- BasicCorePrograme/Program.cs
+++ BasicCorePrograme/Program.cs
@@ -4,25 +4,30 @@ using System;
 
 namespace BasicCorePrograme
 {
-    class SwapNumber
+    class EvenOdd
     {
+
         public static void Main(string[] args)
         {
-            int numA, numB, temp;
-            Console.WriteLine("Input the First Number : ");
-            numA = int.Parse(Console.ReadLine());
-            Console.WriteLine("Input the Second Number : ");
-            numB = int.Parse(Console.ReadLine());
-            temp = numA;
-            numA = numB;
-            numB = temp;
-            Console.Write("\nAfter Swapping : ");
-            Console.Write("\nFirst Number : " + numA);
-            Console.Write("\nSecond Number : " + numB);
-            Console.Read();
-        }    
+
+
+            int number;
+            Console.Write("Enter a Number : ");
+            number = int.Parse(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.Write("Even Number");
+            }
+
+            else
+            {
+                Console.Write("Odd Number");
+            }
+
+
+        }
+
+
     }
 
-
 }
-
